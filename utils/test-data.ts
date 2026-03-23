a0ee044a8ee28569d3355527f6532f1fccd29bcc
PradeepHopkins <pradeephopkins@gmail.com>
2026-03-23 18:33:18 +0530

    Remove Test Data in CI

--- utils/test-data.ts
+++ utils/test-data.ts
@@ -1,6 +1,6 @@
 export const testData = {
-  baseUrl: process.env.BASE_URL || 'https://www.amazon.in',
-  searchTerm: process.env.SEARCH_TERM || 'HP smart tank',
-  productName: process.env.PRODUCT_NAME || 'Smart Tank 589 All-in-One WiFi Colour Printer |Up to 4000 Black & 6000 Colour Prints I Print,Scan & Copy for Home/Office',
-  quantity: process.env.QUANTITY || '2'
+  baseUrl: 'https://www.amazon.in',
+  searchTerm:'HP smart tank',
+  productName: 'Smart Tank 589 All-in-One WiFi Colour Printer |Up to 4000 Black & 6000 Colour Prints I Print,Scan & Copy for Home/Office',
+  quantity: '2'
 };
